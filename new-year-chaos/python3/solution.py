852e4c426874726cfb36a0273c0aec78dbc7f781
Oscar Chang <oscar.chang@Oscars-MacBook-Pro.local>
2019-12-20 11:50:34 -0600

    Solved new-year-chaos

--- new-year-chaos/python3/solution.py
+++ new-year-chaos/python3/solution.py
@@ -7,7 +7,16 @@ def minimumBribes(q):
 	# add d to accumulator
 	# predict next item
 	# if next item is not the predicted value (current value - d) add 1 to accumulator
-	print(q) 
+	
+	result = 0
+	for i in range(len(q) - 1, -1, -1):
+		if (q[i] - (i + 1)) > 2:
+			result = "Too chaotic"
+			break
+		for j in range(max(0, q[1]-3), i):
+			if q[j] > q[i]:
+				result += 1
+	print(result) 
 
 if __name__ == "__main__":
 	lines = [i.strip() for i in sys.stdin.readlines()]
